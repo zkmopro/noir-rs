e8d8086a7bcdd7816b7a24d08ec3ea5ef9e6dfaf
Ya-wen, Jeng <vivi432@yahoo.com.tw>
2025-08-15 14:09:39 +0800

    refactor: remove Android-specific linking logic from build script

--- bb/build.rs
+++ bb/build.rs
@@ -79,29 +79,4 @@ fn main() {
         .args(&["copy-headers.sh", &"./include"])
         .output()
         .unwrap();
-
-    // refer to https://github.com/bbqsrc/cargo-ndk to see how to link the libc++_shared.so file in Android
-    if env::var("CARGO_CFG_TARGET_OS").unwrap() == "android" {
-        android();
-    }
-}
-
-fn android() {
-    println!("cargo:rustc-link-lib=c++_shared");
-
-    if let Ok(output_path) = env::var("CARGO_NDK_OUTPUT_PATH") {
-        let sysroot_libs_path = PathBuf::from(env::var_os("CARGO_NDK_SYSROOT_LIBS_PATH").unwrap());
-        let lib_path = sysroot_libs_path.join("libc++_shared.so");
-        assert!(
-            lib_path.exists(),
-            "Error: Source file {:?} does not exist",
-            lib_path
-        );
-        let dest_dir = Path::new(&output_path).join(env::var("CARGO_NDK_ANDROID_TARGET").unwrap());
-        println!("cargo:rerun-if-changed={}", dest_dir.display());
-        if !dest_dir.exists() {
-            fs::create_dir_all(&dest_dir).unwrap();
-        }
-        fs::copy(lib_path, Path::new(&dest_dir).join("libc++_shared.so")).unwrap();
-    }
 }
